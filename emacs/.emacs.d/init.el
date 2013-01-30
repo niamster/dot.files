89423d4947ea252e47087c7806e1d2308e61db60
Dmytro Milinevskyy <milinevskyy@gmail.com>
2013-01-30 01:50:50 +0100

    Revert "[emacs] autorefresh dired buffers but not others"
    This reverts commit acb3f03c7d4ea2202b5d88772f2b701b89ff92ff.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -607,13 +607,6 @@
   (ediff-buffers first-buffer second-buffer)
   )
 
-; do not auto refresh buffers
-(global-auto-revert-mode nil)
-
-; auto refresh dired, but be quiet about it
-(setq global-auto-revert-non-file-buffers t)
-(setq auto-revert-verbose nil)
-
 ; modules
 
 (require 'whitespace)
