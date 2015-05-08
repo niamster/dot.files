df8029403c39ce82e4a067cb1ce88875b3298381
Dmytro Milinevskyy <milinevskyy@gmail.com>
2015-05-08 20:08:34 +0200

    emace: attempt to use ELPA

--- emacs/.emacs
+++ emacs/.emacs
@@ -2,7 +2,4 @@
   "The root directory of my Emacs configuration.")
 (setq user-emacs-directory (expand-file-name "~/.emacs.d/savefiles/" main-dir))
 
-(setq load-path	(cons "~/.emacs.d" load-path))
-(byte-recompile-directory "~/.emacs.d" 0 nil)
-
 (load "~/.emacs.d/init.el")
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -642,7 +642,29 @@
   (ediff-buffers first-buffer second-buffer)
   )
 
-; modules
+(require 'package) ;; You might already have this line
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+
+;; This should go _after_ package-initialize(bug in package.el?)
+(setq load-path	(cons "~/.emacs.d" load-path))
+(byte-recompile-directory "~/.emacs.d" 0 nil)
+
+;; modules from elpa:
+;; - browse-kill-ring
+;; - dired+
+;; - etags-select
+;; - highlight-parentheses
+;; - highlight-symbol
+;; - idle-highlight-mode
+;; local modules
+;; - ascope
+
+;; Modules initialization and their parameters
 
 (require 'whitespace)
 (setq whitespace-style '(face trailing empty))
