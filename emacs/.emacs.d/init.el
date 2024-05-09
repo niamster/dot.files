b0988371306756017db10390bba473c4c043cf85
Dmytro Milinevskyi <dmytro.milinevskyi@datadoghq.com>
2024-05-09 03:01:06 +0200

    emacs: enable LSP booster

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -781,11 +781,13 @@
 (use-package flycheck)
 
 ;;
+(setenv "LSP_USE_PLISTS" "true")
 (use-package lsp-mode)
 (setq lsp-semgrep-languages '()) ; disable semgrep as it is currently broken
 (setq lsp-keymap-prefix "C-c l")
 (setq lsp-file-watch-threshold 100000)
 (setq lsp-enable-snippet nil)
+(setq lsp-use-plists t)
 ;; See https://github.com/emacs-lsp/lsp-mode/blob/ee58d9eb66f85c2258b9745f721965f0d62071fb/clients/lsp-go.el#L97
 (setq lsp-go-directory-filters [
                                 "-**/bazel-out"
@@ -794,6 +796,36 @@
 ;; Perf tuning, see https://emacs-lsp.github.io/lsp-mode/page/performance/
 (setq gc-cons-threshold 100000000)
 (setq read-process-output-max (* 1024 1024))
+
+;; See https://github.com/blahgeek/emacs-lsp-booster?tab=readme-ov-file#configure-lsp-mode
+(defun lsp-booster--advice-json-parse (old-fn &rest args)
+  "Try to parse bytecode instead of json."
+  (or
+   (when (equal (following-char) ?#)
+     (let ((bytecode (read (current-buffer))))
+       (when (byte-code-function-p bytecode)
+         (funcall bytecode))))
+   (apply old-fn args)))
+(advice-add (if (progn (require 'json)
+                       (fboundp 'json-parse-buffer))
+                'json-parse-buffer
+              'json-read)
+            :around
+            #'lsp-booster--advice-json-parse)
+
+(defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+  "Prepend emacs-lsp-booster command to lsp CMD."
+  (let ((orig-result (funcall old-fn cmd test?)))
+    (if (and (not test?)                             ;; for check lsp-server-present?
+             (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+             lsp-use-plists
+             (not (functionp 'json-rpc-connection))  ;; native json-rpc
+             (executable-find "emacs-lsp-booster"))
+        (progn
+          (message "Using emacs-lsp-booster for %s!" orig-result)
+          (cons "emacs-lsp-booster" orig-result))
+      orig-result)))
+(advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
 
 (setq modes '(go-mode-hook
               rust-mode-hook
